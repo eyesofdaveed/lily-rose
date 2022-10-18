62ecec46fe3ad638e11e3900179b01c848a0f370
Daulet Dauitbek <dauitbek@Daulets-MacBook-Pro.local>
2022-10-18 22:55:35 +0600

    add sale button and fix funeral

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Link from "next/link";
 import Tilt from "react-vanilla-tilt";
+import Image from "next/image";
 
 import { urlFor } from "../lib/client";
 
-const Product = ({ product: { image, name, slug, price } }) => {
+const Product = ({ product: { image, name, slug, price, oldPrice } }) => {
   return (
     <Tilt className="product-card-container">
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
+          {!!oldPrice && (
+            <div className="product-card-sale-wrapper">
+              <Image
+                src="/sale.png"
+                alt="flowers and bouquets for sale"
+                width="74"
+                height="74"
+              />
+            </div>
+          )}
           <img
             src={urlFor(image && image[0])}
             width={250}
