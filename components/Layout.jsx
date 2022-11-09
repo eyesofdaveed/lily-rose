d31ca6667a93515006457c8f047089579858ad3e
Daulet Dauitbek <dauitbek@Daulets-MacBook-Pro.local>
2022-11-09 20:30:10 +0600

    social media tags

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -44,6 +44,21 @@ const Layout = ({ children }) => {
           Monte Sereno,
           Stanford,"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta
+          property="og:title"
+          content="Online flowers and bouquets delivery in Sunnyvale"
+        />
+        <meta
+          property="og:description"
+          content="Wide variety of flowers arrangements  and decorations for Birthday, Marriage Proposal, Marriage, Corporates and Funeral."
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
           rel="apple-touch-icon"
